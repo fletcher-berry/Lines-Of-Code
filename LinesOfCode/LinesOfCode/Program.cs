1aaad1ecd3375d1ea24ae6b19f38820aa5f07308
fletcher-berry <32848772+fletcher-berry@users.noreply.github.com>
2018-10-13 14:21:46 -0500

    added code

---
+++ LinesOfCode/LinesOfCode/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinesOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file path: ");
+            string path = Console.ReadLine();
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("The directory does not exist");
+            }
+            Console.Write("Enter extension (without \".\"): ");
+            string extension = Console.ReadLine();
+            var fileNames = Directory.EnumerateFiles(path, $"*.{extension}", SearchOption.AllDirectories);
+            
+            var sum = 0;
+            foreach(var fileName in fileNames)
+            {
+                var lines = File.ReadLines(fileName).Count();
+                var reducedFileName = Path.GetFileName(fileName);
+                Console.WriteLine($"{reducedFileName}: {lines} lines");
+                sum += lines;
+            }
+            Console.WriteLine($"Total: {sum} lines");
+            Console.WriteLine();
+            Console.WriteLine("press enter to continue.");
+            Console.ReadLine();
+
+        }
+    }
+}
